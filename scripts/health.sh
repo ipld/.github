70b276a1f5173b1a288f148e24d06ed1c2ec32fd
Marten Seemann <martenseemann@gmail.com>
2021-04-01 10:07:30 +0700

    add the script used check the Go repo health

---
+++ scripts/health.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+to_status() {
+  if [[ $1 == 0 ]]; then
+    printf "✅"
+    return
+  fi
+  printf "❌"
+}
+
+check_go_mod_tidy() {
+  if [[ ! -f go.mod || ! -f go.sum ]]; then
+    echo 1
+    return
+  fi
+  cp go.mod go.mod.orig
+  cp go.sum go.sum.orig
+  go mod tidy &> /dev/null
+  diff go.mod go.mod.orig > /dev/null
+  if [[ $? != 0 ]]; then
+    echo 1
+    return
+  fi
+  diff go.sum go.sum.orig > /dev/null
+  echo $?
+}
+
+check_go_fmt() {
+  out=$(gofmt -l . | xargs)
+  if [[ -z "$out" ]]; then
+    echo 0
+    return
+  fi
+  echo 1
+}
+
+for repo in $(node get-repos/index.js | jq -r '.[].target'); do
+  if [[ "$repo" == "ipld/go-ipld-prime" ]]; then 
+    continue
+  fi
+  tmp=$(mktemp -d)
+  pushd $tmp > /dev/null
+  git clone -q https://github.com/$repo .
+  numcommits=$(git rev-list --count --since="Oct 1 2020" --all --no-merges)
+  gomodtidy=$(check_go_mod_tidy)
+  gofmt=$(check_go_fmt)
+  go test -failfast ./... &> /dev/null
+  gotest=$?
+  go test -failfast -race ./... &> /dev/null
+  gotestrace=$?
+  go vet ./... &> /dev/null
+  govet=$?
+  staticcheck ./... &> /dev/null
+  staticcheck=$?
+  echo "$repo $numcommits $(to_status $gomodtidy) $(to_status $gofmt) $(to_status $gotest) $(to_status $gotestrace) $(to_status $govet) $(to_status $staticcheck)"
+  popd > /dev/null
+  rm -rf $tmp
+done
+
